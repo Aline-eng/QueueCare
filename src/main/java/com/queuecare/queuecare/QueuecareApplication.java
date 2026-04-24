46aee99e15cba7ec3e5ed7954af45a48a1b1f079
Aline-eng <alinenzikwinkunda@gmail.com>
2026-04-24 21:36:05 +0200

    Initial commit: Spring Boot project setup with dependencies

---
+++ src/main/java/com/queuecare/queuecare/QueuecareApplication.java
@@ -0,0 +1,13 @@
+package com.queuecare.queuecare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueuecareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueuecareApplication.class, args);
+	}
+
+}
